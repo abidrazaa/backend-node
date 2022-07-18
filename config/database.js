d775427f6821182b42e7dba70c1fb0c5cc7d134d
Abid Raza <abid.neduet@gmail.com>
2022-07-19 01:40:29 +0500

    database

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mogoose = require('mongoose')
 const { DB_CON_STRING } = process.env
 
 module.exports = () => {
-    mogoose.connect(DB_CON_STRING)
+    mogoose.connect("mongodb+srv://abidrazaa:Abcd1234@cluster0.lr2rk.mongodb.net/?retryWrites=true&w=majority")
         .then(() => console.log('DB Connection Successfull'))
         .catch(err => console.log(err.message))
 }
