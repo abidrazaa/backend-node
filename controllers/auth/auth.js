59b95902d847c14086d1d58593d332178d385527
Abid Raza <abid.neduet@gmail.com>
2022-08-04 17:24:12 +0500

    heroku deployment

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -1,5 +1,5 @@
 const userModel = require("../../models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken")
 const JWT_SECRET_KEY = process.env.TOKEN_KEY
 
