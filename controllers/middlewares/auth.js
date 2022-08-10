54a45a8ee14e302ccd3b2256319dc6f68dae6a40
Abid Raza <abid.neduet@gmail.com>
2022-08-10 13:32:56 +0500

    orders

--- controllers/middlewares/auth.js
+++ controllers/middlewares/auth.js
@@ -10,23 +10,29 @@ module.exports.isAdmin = async (req, res, next) => {
     let token = req.headers["x-auth-token"] || req.body.token || req.query.token;
 
     if(token){
-        // decode token with TOKEN key to extract the user
-        const decoded = jwt.verify(token,TOKEN_KEY)
+        try{
+            // decode token with TOKEN key to extract the user
+            const decoded = jwt.verify(token,TOKEN_KEY)
 
-        // saving the current user in req.user
-        req.user = decoded
+            // saving the current user in req.user
+            req.user = decoded
 
-        // checking if the logged in user is ADMIN or not
-        const user = await userModel.findOne({_id : decoded?._id, userType : "ADMIN"})
-            .select("-password")
+            // checking if the logged in user is ADMIN or not
+            const user = await userModel.findOne({_id : decoded?._id, userType : "ADMIN"})
+                .select("-password")
 
-        if(!user){
-            // if not admin
-            return res.send("Insufficient User Permissions")
+            if(!user){
+                // if not admin
+                return res.send("Insufficient User Permissions")
+            }
+
+            // if admin, pass to the next function call
+            return next()
+
+        }catch(error){
+            return res.status(401).json({ msg: "Invalid User Auth Token", err: error.message });     
         }
 
-        // if admin, pass to the next function call
-        return next()
     }
     else{
         return res.status(400).json({ msg: "No Auth Token Found", err: "No Auth Token Found" });
@@ -40,16 +46,24 @@ module.exports.checkAuth = async (req, res, next) => {
         let token = req.headers["x-auth-token"] || req.body.token || req.query.token;
 
         if(token){
-            const decoded = jwt.verify(token,TOKEN_KEY)
-            req.user = decoded
+            try{
 
-            const user = await userModel.findOne({_id : decoded?._id})
-                .select("-password")
+                const decoded = jwt.verify(token,TOKEN_KEY)
+                req.user = decoded
 
-            if(!user){
-                return res.send("Authentication failed")
+                const user = await userModel.findOne({_id : decoded?._id})
+                    .select("-password")
+
+                if(!user){
+                    return res.send("Authentication failed")
+                }
+                return next()
+
+            }catch(error){
+                
+                return res.status(401).json({ msg: "Invalid User Auth Token", err: error.message });     
             }
-            return next()
+        
         }
         else{
             return res.status(400).json({ msg: "No Auth Token Found", err: "No Auth Token Found" });
